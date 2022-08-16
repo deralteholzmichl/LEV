62d091193023351d3fea51147bd4a575472556ae
Aleksandar Milosevic <96289102+Aleks-Memobar@users.noreply.github.com>
2022-08-16 15:52:40 +0200

    datenklasse

--- LEV/src/main/java/datenklasse/Person_RegisterDAO.java
+++ LEV/src/main/java/datenklasse/Person_RegisterDAO.java
@@ -1,18 +1,5 @@
 package datenklasse;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class Person_RegisterDAO {
-    public void insertPerson(Person person) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("abc");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(person);
-        em.flush();
-        em.getTransaction().commit();
-        em.close();
-        emf.close();
-    }
+
 }
